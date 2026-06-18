d978f364a5068454133a0698db41c92599c1248b
Kirsten Roschanski <roschanski@hrz.uni-marburg.de>
2026-06-18 12:35:40 +0200

    first commit

--- src/KirstenroschanskiContaoWiderrufBundle.php
+++ src/KirstenroschanskiContaoWiderrufBundle.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Kirstenroschanski\ContaoWiderrufBundle;
 
-use Kirstenroschanski\ContaoWiderrufBundle\DependencyInjection\KirstenroschanskiWiderrufExtension as WiderrufExtension;
+use Kirstenroschanski\ContaoWiderrufBundle\KirstenroschanskiWiderrufExtension as WiderrufExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
