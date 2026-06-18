e28dbcb1332df56244eeee5d882521c4dc8a7fe9
Kirsten Roschanski <roschanski@hrz.uni-marburg.de>
2026-06-18 13:13:00 +0200

    first commit

--- contao/config/config.php
+++ contao/config/config.php
@@ -12,6 +12,22 @@ $GLOBALS['TL_MODELS']['tl_widerruf'] = RevocationModel::class;
 
 // Notification Center (1.x compatible) custom notification types.
 $GLOBALS['NOTIFICATION_CENTER']['NOTIFICATION_TYPE']['widerruf']['widerruf_form_submit'] = [
+    'email_sender_name' => [
+        'consumer_name',
+        'confirmation_email',
+    ],
+    'email_sender_address' => [
+        'confirmation_email',
+    ],
+    'email_replyTo' => [
+        'confirmation_email',
+    ],
+    'email_recipient_cc' => [
+        'confirmation_email',
+    ],
+    'email_recipient_bcc' => [
+        'confirmation_email',
+    ],
     'email_subject' => [
         'revocation_id',
         'consumer_name',
@@ -49,6 +65,22 @@ $GLOBALS['NOTIFICATION_CENTER']['NOTIFICATION_TYPE']['widerruf']['widerruf_form_
 ];
 
 $GLOBALS['NOTIFICATION_CENTER']['NOTIFICATION_TYPE']['widerruf']['widerruf_status_change'] = [
+    'email_sender_name' => [
+        'consumer_name',
+        'confirmation_email',
+    ],
+    'email_sender_address' => [
+        'confirmation_email',
+    ],
+    'email_replyTo' => [
+        'confirmation_email',
+    ],
+    'email_recipient_cc' => [
+        'confirmation_email',
+    ],
+    'email_recipient_bcc' => [
+        'confirmation_email',
+    ],
     'email_subject' => [
         'revocation_id',
         'status',
